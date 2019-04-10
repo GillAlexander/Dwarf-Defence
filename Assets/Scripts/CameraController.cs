9b06e47c994e63234a7ac6c5a812a424eb60d5df
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2019-04-10 10:28:43 +0200

    First Commit added unfinished camera controlls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panspeed = 12;
+    public float screenBoarderThickness = 10;
+    public Camera camera;
+    public Vector2 mapLimit;
+    float speed = 5;
+    //float cameraRotateSpeed = 1;
+    public float scrollspeed = 20000;
+    float minimumHeight = 8, maximumHeight = 25;
+
+    //float yaw = 0.0f, pitch = 0.0f;
+
+    float test = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - screenBoarderThickness){
+            //position.z += panspeed * Time.deltaTime;
+
+            //position += Vector3.forward * speed * Time.deltaTime;
+            //camera.transform.Translate(Vector3.forward * Time.deltaTime);
+            transform.position += this.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= screenBoarderThickness)
+        {
+            position.z -= panspeed * Time.deltaTime;
+
+        }
+        if (Input.GetKey("d"))
+        {
+            position.x += panspeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            position.x -= panspeed * Time.deltaTime;
+        }
+
+        position.x = Mathf.Clamp(position.x, -mapLimit.x, mapLimit.x);
+        position.z = Mathf.Clamp(position.z, -mapLimit.y, mapLimit.y);
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        position.y -= scroll * scrollspeed * Time.deltaTime;
+        position.y = Mathf.Clamp(position.y, minimumHeight, maximumHeight);
+        transform.position = position;
+
+
+        
+        if (Input.mousePosition.x >= Screen.width - screenBoarderThickness)
+        {
+            test++;
+            transform.rotation = Quaternion.Euler(50, test, 0);
+        }
+
+        if (Input.mousePosition.x <= screenBoarderThickness)
+        {
+            test--;
+            transform.rotation = Quaternion.Euler(50, test, 0);
+        }
+    }   
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject spawn;
+    string name;
+    void Start(){
+        
+    }
+    
+    void Update(){
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.distance);
+            }
+            
+        }
+
+        
+    }
+}
