478ea460957aba8c70aa63bb8134989c499269b5
GillAlexander <jonte962010@hotmail.com>
2019-04-22 11:47:11 +0200

    Basic statemachine, camera complete

--- Assets/Script/CameraControls.cs
+++ Assets/Script/CameraControls.cs
@@ -1,54 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControls : MonoBehaviour
-{
+public class CameraControls : MonoBehaviour {
     public float movementSpeed = 0.1f;
     public float rotationSpeed = 4f;
     public float smoothness = 0.85f;
+    public float screenBoarderThickness = 10;
 
-    Vector3 targetPosition;
-    
     public Quaternion targetRotation;
     float targetRotationY;
     float targetRotationX;
+    float floatY = 1;
 
-    // Use this for initialization
-    void Start()
-    {
-        targetPosition = transform.position;
+    void Start() {
         targetRotation = transform.rotation;
         targetRotationY = transform.localRotation.eulerAngles.y;
         targetRotationX = transform.localRotation.eulerAngles.x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if( Input.GetKey( KeyCode.W ) )
-            targetPosition += transform.forward * movementSpeed;
-        if( Input.GetKey( KeyCode.A ) )
-            targetPosition -= transform.right * movementSpeed;
-        if( Input.GetKey( KeyCode.S ) )
-            targetPosition -= transform.forward * movementSpeed;
-        if( Input.GetKey( KeyCode.D ) )
-            targetPosition += transform.right * movementSpeed;
-        if( Input.GetKey( KeyCode.Q ) )
-            targetPosition -= transform.up * movementSpeed;
-        if( Input.GetKey( KeyCode.E ) )
-            targetPosition += transform.up * movementSpeed;
-
-        if( Input.GetMouseButton( 2 ) )
-        {
+    void Update() {
+        if (Input.GetMouseButton(2)) {
             Cursor.visible = false;
-            targetRotationY += Input.GetAxis( "Mouse X" ) * rotationSpeed;
-            targetRotationX -= Input.GetAxis( "Mouse Y" ) * rotationSpeed;
-            targetRotation = Quaternion.Euler( targetRotationX, targetRotationY, 0.0f );
+            targetRotationY += Input.GetAxis("Mouse X") * rotationSpeed;
+            targetRotationX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
         }
         else
             Cursor.visible = true;
 
-        transform.position = Vector3.Lerp( transform.position, targetPosition, ( 1.0f - smoothness ) );
-        transform.rotation = Quaternion.Lerp( transform.rotation, targetRotation, ( 1.0f - smoothness ) );
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, (1.0f - smoothness));
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, (1.0f - smoothness));
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - screenBoarderThickness) {
+            transform.position += Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up) * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.right, Vector3.up) * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= screenBoarderThickness) {
+            transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.forward, Vector3.up) * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up) * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.position -= transform.up * movementSpeed;
+        }
+            
+        if (Input.GetKey(KeyCode.E)) {
+            transform.position += transform.up * movementSpeed;
+        }
+
+        if (Input.mousePosition.x >= Screen.width - screenBoarderThickness) {
+            targetRotationY++;
+            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        }
+
+        if (Input.mousePosition.x <= screenBoarderThickness) {
+            targetRotationY--;
+            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        }
+
     }
 }
