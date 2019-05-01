7949138ed4d9146608267858c8869ef71d8c067a
GillAlexander <jonte962010@hotmail.com>
2019-05-01 23:18:18 +0200

    NEW CAVE SHIT. UPDATED GAMECONTROLLER

--- Assets/Script/MyScript/CameraControls.cs
+++ Assets/Script/MyScript/CameraControls.cs
@@ -36,7 +36,9 @@ public class CameraControls : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, (1.0f - smoothness));
         //|| Input.mousePosition.y >= Screen.height - screenBoarderThickness
         //|| Input.mousePosition.y <= screenBoarderThickness
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - screenBoarderThickness)
+        //|| Input.mousePosition.y >= Screen.height - screenBoarderThickness
+        //|| Input.mousePosition.y <= screenBoarderThickness
+        if (Input.GetKey(KeyCode.W) )
         {
             transform.position += Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up) * movementSpeed * Time.deltaTime;
         }
@@ -44,7 +46,7 @@ public class CameraControls : MonoBehaviour
         {
             transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.right, Vector3.up) * movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= screenBoarderThickness)
+        if (Input.GetKey(KeyCode.S) )
         {
             transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.forward, Vector3.up) * movementSpeed * Time.deltaTime;
         }
@@ -83,17 +85,17 @@ public class CameraControls : MonoBehaviour
 
         }
 
-        if (Input.mousePosition.x >= Screen.width - screenBoarderThickness)
-        {
-            targetRotationY++;
-            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
-        }
+        //if (Input.mousePosition.x >= Screen.width - screenBoarderThickness)
+        //{
+        //    targetRotationY++;
+        //    targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        //}
 
-        if (Input.mousePosition.x <= screenBoarderThickness)
-        {
-            targetRotationY--;
-            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
-        }
+        //if (Input.mousePosition.x <= screenBoarderThickness)
+        //{
+        //    targetRotationY--;
+        //    targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        //}
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             movementSpeed *= 2;
--- Assets/Script/MyScript/GameController.cs
+++ Assets/Script/MyScript/GameController.cs
@@ -6,30 +6,75 @@ using UnityEngine.AI;
 public class GameController : MonoBehaviour
 {
     public Transform treasureChest;
-    public List<Troll> Trolls = new List<Troll>();
-    public List<Goblin> Goblins = new List<Goblin>();
-    public List<Dwarf> Dwarfs = new List<Dwarf>();
-    //A list that will hold all enemies
-    public List<Transform> dwarfTransform = new List<Transform>();
+    public GameObject[] trollsArray;
+    public List<GameObject> trollsList;
     public List<Transform> trollTransform = new List<Transform>();
+    public List<Troll> trollScriptList = new List<Troll>();
+
+    public GameObject[] dwarfsArray;
+    public List<GameObject> dwarfsList;
+    public List<Transform> dwarfTransform = new List<Transform>();
+    public List<Dwarf> dwarfScriptList = new List<Dwarf>();
+
+
+
 
     void Start() {
         //Add the enemies we have
+        trollsArray = GameObject.FindGameObjectsWithTag("Troll");
+        dwarfsArray = GameObject.FindGameObjectsWithTag("Dwarf");
+
+        dwarfsList.AddRange(dwarfsArray);
+        trollsList.AddRange(trollsArray);
+
+        for (int i = 0; i < dwarfsList.Count; i++)
+        {
+            dwarfTransform.Add(dwarfsList[i].transform);
+            dwarfScriptList.Add(dwarfsList[i].GetComponent<Dwarf>());
+        }
+        for (int i = 0; i < trollsList.Count; i++)
+        {
+            trollTransform.Add(trollsList[i].transform);
+            trollScriptList.Add(trollsList[i].GetComponent<Troll>());
+        }
     }
 
-    void Update()
-    {
-        for (int i = dwarfTransform.Count - 1; i > -1; i--) {
-            if (dwarfTransform[i] == null)
+    void Update() {
+        for (int i = 0; i < dwarfsList.Count; i++)
+        {
+            if (dwarfsList[i] != null)
+            {
+                dwarfTransform[i] = dwarfsList[i].transform;
+            }
+            else
+            {
+                dwarfsList.RemoveAt(i);
                 dwarfTransform.RemoveAt(i);
+                Debug.Log("Debuged list");
+            }
         }
-        for (int i = 0; i < Trolls.Count; i++) {
-            if (Trolls[i] == null)
-                Trolls.RemoveAt(i);
+        for (int i = 0; i < trollsList.Count; i++)
+        {
+            if (trollsList[i] != null)
+            {
+                trollTransform[i] = trollsList[i].transform;
+            }
+            else
+            {
+                trollsList.RemoveAt(i);
+                trollTransform.RemoveAt(i);
+            }
         }
-        for (int i = 0; i < Goblins.Count; i++) {
-            if (Goblins[i] == null)
-                Goblins.RemoveAt(i);
+
+
+
+        //Null check
+        for (int i = 0; i < trollsList.Count; i++)
+        {
+            if (trollsList[i] == null)
+            {
+                trollsList.RemoveAt(i);
+            }
         }
         for (int i = 0; i < trollTransform.Count; i++)
         {
@@ -38,20 +83,163 @@ public class GameController : MonoBehaviour
                 trollTransform.RemoveAt(i);
             }
         }
+        for (int i = 0; i < trollScriptList.Count; i++)
+        {
+            if (trollScriptList[i] == null)
+            {
+                trollScriptList.RemoveAt(i);
+            }
+        }
+        for (int i = 0; i < dwarfTransform.Count; i++)
+        {
+            if (dwarfTransform[i] == null)
+            {
+                dwarfTransform.RemoveAt(i);
+                Debug.Log("Debuged transform");
+            }
+        }
+        for (int i = 0; i < dwarfsList.Count; i++)
+        {
+            if (dwarfsList[i] == null)
+            {
+                dwarfsList.RemoveAt(i);
+                Debug.Log("Debuged list");
+            }
+        }
+        for (int i = 0; i < trollScriptList.Count; i++)
+        {
+            if (trollScriptList[i] == null)
+            {
+                trollScriptList.RemoveAt(i);
+                Debug.Log("Debuged scriptlist");
+            }
+        }
 
         //Update all enemies to see if they should change state and move/attack player
-        for (int i = 0; i < Trolls.Count; i++)
+        for (int i = 0; i < trollScriptList.Count; i++)
         {
-            Trolls[i].UpdateState(treasureChest, dwarfTransform);
+            if (trollScriptList[i] != null)
+            {
+                trollScriptList[i].UpdateState(treasureChest, dwarfTransform);
+            }
+            else
+            {
+                trollScriptList.RemoveAt(i);
+            }
         }
-        for (int i = 0; i < Goblins.Count; i++) {
-            Goblins[i].UpdateState(treasureChest, dwarfTransform);
-        }
-        for (int i = 0; i < Dwarfs.Count; i++)
+        for (int i = 0; i < dwarfScriptList.Count; i++)
         {
-            Dwarfs[i].UpdateState(treasureChest, trollTransform);
+            dwarfScriptList[i].UpdateState(treasureChest, trollTransform);
         }
-
     }
 }
 
+//void Start() {
+//    //Add the enemies we have
+//    trollsArray = GameObject.FindGameObjectsWithTag("Troll");
+//    dwarfsArray = GameObject.FindGameObjectsWithTag("Dwarf");
+
+//    dwarfsList.AddRange(dwarfsArray);
+//    trollsList.AddRange(trollsArray);
+
+//    for (int i = 0; i < dwarfsArray.Length; i++)
+//    {
+//        dwarfTransform.Add(dwarfsList[i].transform);
+//        dwarfScriptList.Add(dwarfsList[i].GetComponent<Dwarf>());
+//    }
+//    for (int i = 0; i < trollsArray.Length; i++)
+//    {
+//        trollTransform.Add(trollsList[i].transform);
+//        trollScriptList.Add(trollsList[i].GetComponent<Troll>());
+//    }
+//}
+
+//void Update() {
+//    for (int i = 0; i < dwarfsList.Count; i++)
+//    {
+//        if (dwarfsList[i] != null)
+//        {
+//            dwarfTransform[i] = dwarfsList[i].transform;
+//        }
+//        else
+//        {
+//            dwarfsList.RemoveAt(i);
+//            Debug.Log("Debuged list");
+//        }
+//    }
+//    for (int i = 0; i < trollsList.Count; i++)
+//    {
+//        if (trollTransform[i] != null)
+//        {
+//            trollTransform[i] = trollsList[i].transform;
+//        }
+//        else
+//        {
+//            trollTransform.RemoveAt(i);
+//        }
+//    }
+
+//    //Update all enemies to see if they should change state and move/attack player
+//    for (int i = 0; i < trollScriptList.Count; i++)
+//    {
+//        if (trollScriptList[i] != null)
+//        {
+//            trollScriptList[i].UpdateState(treasureChest, dwarfTransform);
+//        }
+//        else
+//        {
+//            trollScriptList.RemoveAt(i);
+//        }
+//    }
+//    for (int i = 0; i < dwarfScriptList.Count; i++)
+//    {
+//        dwarfScriptList[i].UpdateState(treasureChest, trollTransform);
+//    }
+
+//    //Null check
+//    for (int i = 0; i < trollsList.Count; i++)
+//    {
+//        if (trollsList[i] == null)
+//        {
+//            trollsList.RemoveAt(i);
+//        }
+//    }
+//    for (int i = 0; i < trollTransform.Count; i++)
+//    {
+//        if (trollTransform[i] == null)
+//        {
+//            trollTransform.RemoveAt(i);
+//        }
+//    }
+//    for (int i = 0; i < trollScriptList.Count; i++)
+//    {
+//        if (trollScriptList[i] == null)
+//        {
+//            trollScriptList.RemoveAt(i);
+//        }
+//    }
+//    for (int i = 0; i < dwarfTransform.Count; i++)
+//    {
+//        if (dwarfTransform[i] == null)
+//        {
+//            dwarfTransform.RemoveAt(i);
+//            Debug.Log("Debuged transform");
+//        }
+//    }
+//    for (int i = 0; i < dwarfsList.Count; i++)
+//    {
+//        if (dwarfsList[i] == null)
+//        {
+//            dwarfsList.RemoveAt(i);
+//            Debug.Log("Debuged list");
+//        }
+//    }
+//    for (int i = 0; i < dwarfScriptList.Count; i++)
+//    {
+//        if (trollScriptList[i] == null)
+//        {
+//            dwarfScriptList.RemoveAt(i);
+//            Debug.Log("Debuged scriptlist");
+//        }
+//    }
+//}
