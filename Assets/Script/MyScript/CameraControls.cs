f2bb91e523ec2c8d52da644440f0fa2f6d0e252e
GillAlexander <jonte962010@hotmail.com>
2019-05-06 22:20:02 +0200

    removed scripts

--- Assets/Script/MyScript/CameraControls.cs
+++ Assets/Script/MyScript/CameraControls.cs
@@ -38,7 +38,7 @@ public class CameraControls : MonoBehaviour
         //|| Input.mousePosition.y <= screenBoarderThickness
         //|| Input.mousePosition.y >= Screen.height - screenBoarderThickness
         //|| Input.mousePosition.y <= screenBoarderThickness
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - screenBoarderThickness )
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up) * movementSpeed * Time.deltaTime;
         }
@@ -46,7 +46,7 @@ public class CameraControls : MonoBehaviour
         {
             transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.right, Vector3.up) * movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= screenBoarderThickness )
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.ProjectOnPlane(-Camera.main.transform.forward, Vector3.up) * movementSpeed * Time.deltaTime;
         }
@@ -85,17 +85,17 @@ public class CameraControls : MonoBehaviour
 
         }
 
-        if (Input.mousePosition.x >= Screen.width - screenBoarderThickness)
-        {
-            targetRotationY++;
-            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
-        }
+        //if (Input.mousePosition.x >= Screen.width - screenBoarderThickness)
+        //{
+        //    targetRotationY++;
+        //    targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        //}
 
-        if (Input.mousePosition.x <= screenBoarderThickness)
-        {
-            targetRotationY--;
-            targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
-        }
+        //if (Input.mousePosition.x <= screenBoarderThickness)
+        //{
+        //    targetRotationY--;
+        //    targetRotation = Quaternion.Euler(targetRotationX, targetRotationY, 0.0f);
+        //}
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             movementSpeed *= 2;
